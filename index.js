ca5c4566de5a51be0f88a059332e87934ba41bd5
DoanDuyChinh <doanchinhit21@gmail.com>
2025-02-13 23:13:31 +0700

    config

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-// const port = 3001;
-// app.listen(port, () =>
-//   console.log(`server is listening at http://localhost:${port}`),
-// );
+const port = process.env.PORT || 3001;
+app.listen(port, () =>
+  console.log(`Server is running at http://localhost:${port}`)
+);
