3b9ef4c6fdb3ef87027d198eba4c93708453ec96
DoanDuyChinh <doanchinhit21@gmail.com>
2025-02-17 10:48:18 +0700

    notification read

--- controllers/notification.controller.js
+++ controllers/notification.controller.js
@@ -17,5 +17,61 @@ module.exports = {
                 data: notifications,
               });
         }
-    },    
+    },   
+    
+    getNotiByUserIdUnRead: async (req, res) => {
+        const { user_id } = req.params;
+        try {
+            const notifications = await Notification.find({ user_id, read: false }).sort({ created_at: -1 });
+            return res.status(200).json({
+                status: 200,
+                message: "Notifications retrieved successfully",
+                data: notifications,
+              });
+        } catch (error) {
+            return res.status(200).json({
+                status: 200,
+                message: "Notifications retrieved successfully",
+                data: notifications,
+              });
+        }
+    },
+
+    markAsRead: async (req, res) => {
+        const { id } = req.params;
+        try {
+            const notification = await Notification.findById(id);
+            notification.read = true;
+            await notification.save();
+            return res.status(200).json({
+                status: 200,
+                message: "Notification marked as read successfully",
+              });
+        } catch (error) {
+            return res.status(400).json({
+                status: 400,
+                message: "Error marking notification as read",
+              });
+        }
+    },
+
+    markAsReadAll: async (req, res) => {
+        const { user_id } = req.params;
+        try {
+            const notifications = await Notification.find({ user_id, read: false });
+            notifications.forEach(async (notification) => {
+                notification.read = true;
+                await notification.save();
+            });
+            return res.status(200).json({
+                status: 200,
+                message: "All notifications marked as read successfully",
+              });
+        } catch (error) {
+            return res.status(400).json({
+                status: 400,
+                message: "Error marking all notifications as read",
+              });
+        }
+    }
 };
--- router/notification.js
+++ router/notification.js
@@ -5,5 +5,7 @@ const authMiddleware = require('../middlewares/auth.middleware');
 const notificationController = require('../controllers/notification.controller');
 
 router.get("/:user_id", authMiddleware, notificationController.getNotificationByUserId);
-
+router.get("/unread/:user_id", authMiddleware, notificationController.getNotiByUserIdUnRead);
+router.post("/mark-as-read/:id", authMiddleware, notificationController.markAsRead);
+router.post("/mark-as-read-all/:user_id", authMiddleware, notificationController.markAsReadAll);
 module.exports = router;
